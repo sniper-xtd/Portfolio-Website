e0947fdd3f69d545678e716f75bd14a26aec0327
sniper-xtd <145884748+sniper-xtd@users.noreply.github.com>
2024-06-21 18:34:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const header = document.querySelector("header");
-window.addEventListener ("scroll", function() { 
-    header.classList.toggle ("sticky", window.scrollY > 100);
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.onclick = () = {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 };
 
-window.scroll = () = {
+window.onscroll = () => {
     menu.classList.remove('bx-x');
-    navlist.classList.remove('open')
+    navlist.classList.remove('open');
 };
